40dde0ff468afe861c021665d02dcdd53284259d
dmcdaniel90 <dmcdaniel9@gmail.com>
2025-03-27 10:17:55 +0000

    Fixed issue #3 - Hint revealing entire word

--- src/components/Heading/Hint.tsx
+++ src/components/Heading/Hint.tsx
@@ -2,14 +2,29 @@ import { Typography } from '@mui/material';
 import { Pokemon } from '../../types/types';
 import { memo } from 'react';
 
-function Hint(props: { currentPokemon: Pokemon | undefined, showHint: boolean }) {
-    const { currentPokemon, showHint } = props;
-    return (
-        <Typography variant="h5" sx={{ color: 'white' }} fontFamily={'Roboto'}>
-            {/* TODO Conditionally display the name of the current pokemon if showHint is true */}
-            {showHint && currentPokemon ? `Hint: ${currentPokemon.name.charAt(0).toUpperCase() + currentPokemon.name.slice(1)}` : ''}
-        </Typography>
-    );
+function Hint(props: {
+  currentPokemon: Pokemon | undefined;
+  showHint: boolean;
+}) {
+  const { currentPokemon, showHint } = props;
+
+  if (!currentPokemon) {
+    return null;
+  }
+
+  const formattedName =
+    currentPokemon.name.charAt(0).toUpperCase() +
+    currentPokemon.name.slice(1).replace(/./g, '*');
+
+  return (
+    <Typography
+      variant='h5'
+      sx={{ color: 'white' }}
+      fontFamily={'Roboto'}>
+      {/* TODO Conditionally display the name of the current pokemon if showHint is true */}
+      {showHint && currentPokemon ? `Hint: ${formattedName}` : ''}
+    </Typography>
+  );
 }
 
 export default memo(Hint);
